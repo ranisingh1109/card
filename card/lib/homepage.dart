bf0d898e7b68e18b4b3445f5ad3fd4de5041e54f
rahul_kgarrcz <ranikumari.edugaon@gamil.com>
2024-01-02 11:39:11 +0530

    project is created

--- card/lib/homepage.dart
+++ card/lib/homepage.dart
@@ -1,6 +1,4 @@
-
-
-
+import 'package:flutter/material.dart';
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
--- card/lib/main.dart
+++ card/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-
+import'package:card/homepage.dart';
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home:MyHomePage(),
     );
   }
 }
