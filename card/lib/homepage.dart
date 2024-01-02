332dcb68a2a2424683be2bd41ca03834daf125e8
rahul_kgarrcz <ranikumari.edugaon@gamil.com>
2024-01-02 11:57:46 +0530

    project is created

--- card/lib/homepage.dart
+++ card/lib/homepage.dart
@@ -1,48 +1,45 @@
+
 import 'package:flutter/material.dart';
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
 
+class homepage extends StatefulWidget {
+  const homepage({super.key, required this.title});
   final String title;
-
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<homepage> createState() => _homepageState();
 }
-
-class _MyHomePageState extends State<MyHomePage> {
+class _homepageState extends State<homepage> {
   int _counter = 0;
 
   void _incrementCounter() {
     setState(() {
-
       _counter++;
     });
   }
-
   @override
   Widget build(BuildContext context) {
 
     return Scaffold(
-
-    ),
-    body: Center(
-
-    mainAxisAlignment: MainAxisAlignment.center,
-    children: <Widget>[
-    const Text(
-    'You have pushed the button this many times:',
-    ),
-    Text(
-    '$_counter',
-    style: Theme.of(context).textTheme.headlineMedium,
-    ),
-    ],
-    ),
-    ),
-    floatingActionButton: FloatingActionButton(
-    onPressed: _incrementCounter,
-    tooltip: 'Increment',
-    child: const Icon(Icons.add),
-    ), // This trailing comma makes auto-formatting nicer for build methods.
+      appBar: AppBar(title: Text("DEMO"),backgroundColor: Colors.black38,),
+    body: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+           children: [
+             Center(
+               child: Text(
+                   'RANI KUMARI:',
+                ),
+             ),
+            Text(
+           '$_counter',
+           style: Theme.of(context).textTheme.headlineMedium,
+          ),
+        ],
+          ),
+      floatingActionButton: FloatingActionButton(
+       onPressed: _incrementCounter,
+       tooltip: 'Increment',
+        child:  Icon(Icons.add),
+        ),
+         // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
--- card/lib/main.dart
+++ card/lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
-import'package:card/homepage.dart';
+import 'homepage.dart';
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home:MyHomePage(),
+      home: homepage(title: 'Tyxrrrrtucyifvugli',),
     );
   }
 }
